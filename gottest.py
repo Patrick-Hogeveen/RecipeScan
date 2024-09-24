259dd849c08103624e740c431e648712fab797aa
Patrick-Hogeveen <root@DESKTOP-O7SSBS5>
2024-09-24 00:14:27 -0400

    cleaning

--- gottest.py
+++ gottest.py
@@ -5,7 +5,7 @@ model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True,
 low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenzier.eos_token_id)
 model = model.eval().cuda()
 
-image_file = 'IMG_20240922_152049.jpg'
+image_file = 'clean.jpg'
 
 res = model.chat(tokenzier, image_file, ocr_type='ocr')
 print(res)
